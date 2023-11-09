f843505a2e906329812e31ab58c7e2af03c7d83d
Mehdi <belazzoug.mehdi@hotmail.com>
2023-11-09 18:00:09 +0100

    Ajout de la page de connexion fonctionnelle

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,40 +1,36 @@
 const repEmail = document.querySelector("#email");
 const repPassword = document.querySelector("#password");
 const loginButton = document.querySelector("#loginButton");
-const error = document.querySelector("#error");
-
-let userAuth = {
-  userId: "",
-  token: "",
-};
+const invalid = document.querySelector("#error");
 
 loginButton.addEventListener("click", (e) => {
   e.preventDefault();
+
+  const email = repEmail.value;
+  const password = repPassword.value;
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        error.classList.remove("hide");
+        throw new Error("Impossible de connecter l'utilisateur");
+      }
+      error.classList.add("hide");
+      return response.json();
+    })
+
+    .then((data) => {
+      window.location.href = "index.html";
+      sessionStorage.setItem("token", data.token);
+      sessionStorage.setItem("userId", data.userId);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
-
-const email = repEmail.value;
-const password = repPassword.value;
-
-fetch("http://localhost:5678/api/users/login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ email, password }),
-})
-  .then((response) => {
-    if (!response.ok) {
-      error.classList.remove("hide");
-      throw new Error("Impossible de connecter l'utilisateur");
-    }
-    error.classList.add("hide");
-    return response.json();
-  })
-  .then((data) => {
-    userAuth.userId = data.userId;
-    userAuth.token = data.token;
-    console.log(userAuth.token, userAuth.userId);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
